e018d4a29134e79fbb65f40e17eb6fbe4e4d3623
dvlprwchoi <dvlpr.wchoi@gmail.com>
2022-01-03 12:34:48 -0600

    Separate home.css

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { collection, getDocs } from 'firebase/firestore';
 import { db } from '../../firebase-config';
+import './Home.css';
 
 function Home() {
   const [postList, setPostList] = useState([]);
