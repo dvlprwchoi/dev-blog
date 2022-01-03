d628355e250d8b2ca02365df4ef34092713d5c9f
dvlprwchoi <dvlpr.wchoi@gmail.com>
2022-01-02 20:31:08 -0600

    Add create post form and css

--- src/components/Create/Create.js
+++ src/components/Create/Create.js
@@ -1,5 +1,37 @@
 function Create() {
-  return <div className="create main">This is Create</div>;
+  return (
+    <div className="create main">
+      <div className="create-title">
+        <h2>Create a post</h2>
+      </div>
+      <div className="create-post-inputs">
+        <form>
+          <div className="input-title-div">
+            <label htmlFor="title">Title: </label>
+            <input
+              className="post-title-input-box"
+              type="text"
+              name="title"
+              placeholder="Add title..."
+              required
+            />
+          </div>
+          <div className="input-text-div">
+            <label htmlFor="post">Post: </label>
+            <textarea
+              className="post-text-input-area"
+              name="post"
+              placeholder="Add post..."
+              required
+            ></textarea>
+          </div>
+          <div className="submit-button-div">
+            <button className="submit-button button">Submit</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Create;
